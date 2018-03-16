fc9795d077162360b7898c00ed163b21fe24cae6
jaboyles <jacobboyles0115@gmail.com>
2018-03-16 13:42:48 -0400

    pls work

--- src/main/java/com/revature/beans/Sender.java
+++ src/main/java/com/revature/beans/Sender.java
@@ -3,13 +3,18 @@ package com.revature.beans;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 //This sends a message, not about the money
 public class Sender {
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
 	
-	public void sendTrex(TRex trex) {
-		this.rabbitTemplate.convertAndSend("trex", trex);
+	public void sendTrex(TRex trex) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(trex);
+		this.rabbitTemplate.convertAndSend("trex", json);
 	}
 }
