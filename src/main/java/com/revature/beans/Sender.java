1638abdf7d264719d70dbcdc8d98fd84faf37100
jaboyles <jacobboyles0115@gmail.com>
2018-03-16 11:14:09 -0400

    pls work

--- src/main/java/com/revature/beans/Sender.java
+++ src/main/java/com/revature/beans/Sender.java
@@ -2,7 +2,6 @@ package com.revature.beans;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 //This sends a message, not about the money
@@ -11,7 +10,7 @@ public class Sender {
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
-	@Scheduled(fixedDelay = 1000L)
+	
 	public void sendTrex(TRex trex) {
 		this.rabbitTemplate.convertAndSend("trex", trex);
 	}
