1a5ee18ef6b85cb645bc50d8db1182cadfe64ee8
jaboyles <jacobboyles0115@gmail.com>
2018-03-16 11:19:31 -0400

    pls work

--- src/main/java/com/revature/beans/Sender.java
+++ src/main/java/com/revature/beans/Sender.java
@@ -2,10 +2,8 @@ package com.revature.beans;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 //This sends a message, not about the money
-@Component
 public class Sender {
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
